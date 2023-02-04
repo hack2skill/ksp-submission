a9b5840f286c3f7a63902e1768fff6102513f19f
Chanakyha <vetrichanakyha2003@gmail.com>
2023-02-04 16:52:15 +0530

    Getting all the data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,29 @@
-import type { NextPage } from "next";
-import { AiOutlineSearch } from "react-icons/ai";
 import { BsFillPersonFill } from "react-icons/bs";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import JSONfile from "../icjs.json";
 
-const Home: NextPage = () => {
+const Home = () => {
+  const [searchData, setSearchData] = useState({});
+  const [jsonData, setJsonData] = useState(JSONfile);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const filteredData = JSONfile.filter((data) =>
+      data.Person_Name?.toLowerCase().includes(searchData.name?.toLowerCase())
+    );
+    setJsonData(filteredData);
+
+    if (searchData.fatherName) {
+      const filteredData = JSONfile.filter((data) =>
+        data.Father_Name?.toLowerCase().includes(
+          searchData.fatherName?.toLowerCase()
+        )
+      );
+      setJsonData(filteredData);
+    }
+  }, [searchData]);
+
   return (
     <div className="">
       <Head>
@@ -17,6 +37,10 @@ const Home: NextPage = () => {
             <p className="text-sm font-bold flex-1">Name:</p>
             <div className="px-4 w-full flex items-center py-2 border border-black rounded-md">
               <input
+                onChange={(e) =>
+                  setSearchData({ ...searchData, name: e.target.value })
+                }
+                value={searchData.name}
                 className="outline-none bg-transparent w-full"
                 type="text"
               />
@@ -27,6 +51,10 @@ const Home: NextPage = () => {
             <p className="text-sm font-bold flex-1">Father's Name:</p>
             <div className="px-4 w-full flex items-center py-2 border border-black rounded-md">
               <input
+                onChange={(e) =>
+                  setSearchData({ ...searchData, fatherName: e.target.value })
+                }
+                value={searchData.fatherName}
                 className="outline-none bg-transparent w-full"
                 type="text"
               />
@@ -44,6 +72,10 @@ const Home: NextPage = () => {
             </div>
             <div className="px-4 w-full flex items-center py-2 border border-black rounded-md">
               <input
+                onChange={(e) =>
+                  setSearchData({ ...searchData, fatherName: e.target.value })
+                }
+                value={searchData.fatherName}
                 className="outline-none bg-transparent w-full"
                 placeholder="To"
                 type="number"
@@ -62,7 +94,7 @@ const Home: NextPage = () => {
             </div>
           </div>
           <div className="flex space-x-5">
-            <div className="px-3 py-2 bg-blue-500 text-white rounded-md shadow-lg active:scale-95 ease-out duration-100">
+            <div className="cursor-pointer px-3 py-2 bg-blue-500 text-white rounded-md shadow-lg active:scale-95 ease-out duration-100">
               <label className="flex flex-col">
                 <input
                   type="file"
@@ -72,7 +104,7 @@ const Home: NextPage = () => {
                 Upload Photo
               </label>
             </div>
-            <div className="px-3 py-2 bg-green-500 text-white rounded-md shadow-lg active:scale-95 ease-out duration-100">
+            <div className="cursor-pointer px-3 py-2 bg-green-500 text-white rounded-md shadow-lg active:scale-95 ease-out duration-100">
               <label className="flex flex-col">
                 <input
                   type="file"
@@ -83,10 +115,16 @@ const Home: NextPage = () => {
               </label>
             </div>
           </div>
-          <button className="px-3 py-2 bg-red-500 text-white rounded-md shadow-lg active:scale-95 ease-out duration-100">
+          <button
+            onClick={() => setShow(!show)}
+            className="px-3 py-2 bg-red-500 text-white rounded-md shadow-lg active:scale-95 ease-out duration-100"
+          >
             Search
           </button>
         </div>
+        <code className={!show && "hidden"}>
+          <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+        </code>
       </main>
     </div>
   );
