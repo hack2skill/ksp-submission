4a2788b91c94a0bc3be7ae2d46ca0d4d2cbf72fd
reeshaa <shenoyreesha@gmail.com>
2023-02-05 04:39:36 +0530

    ugh

---
+++ web-app/src/components/UpiInfo.js
@@ -0,0 +1,25 @@
+import { Box, Card, List, ListItem, OrderedList } from '@chakra-ui/react';
+
+export default function UpiInfo({ input = 'xxx', type = 'phone' }) {
+  const phone = ['paytm', 'ybl', 'axl', 'ibl'];
+  const email = ['okicici', 'okaxis', 'okhdfcbank', 'oksbi'];
+  return (
+    <>
+      <Box>You can check the following for the existing UPI ids</Box>
+
+      <OrderedList>
+        {type == 'phone' &&
+          phone.map(e => {
+            return <ListItem>{input + '@' + e}</ListItem>;
+          })}
+      </OrderedList>
+
+      <OrderedList>
+        {type == 'email' &&
+          email.map(e => {
+            return <ListItem>{input + '@' + e}</ListItem>;
+          })}
+      </OrderedList>
+    </>
+  );
+}
--- web-app/src/screens/Dashboard.js
+++ web-app/src/screens/Dashboard.js
@@ -2,24 +2,19 @@ import { SearchIcon } from '@chakra-ui/icons';
 import {
   Box,
   Button,
-  Card,
-  CardBody,
-  CardHeader,
   Center,
-  Grid,
-  Heading,
   HStack,
   Input,
   InputGroup,
   InputLeftElement,
   Link,
-  LinkBox,
-  LinkOverlay,
   Text,
   useColorModeValue,
   useDisclosure,
 } from '@chakra-ui/react';
 import { useState } from 'react';
+import BottomSearch from '../components/BottomSearch';
+import PhoneSearch from '../components/PhoneSearch';
 
 const Links = ['Dashboard', 'Projects', 'Team'];
 
@@ -41,6 +36,7 @@ const NavLink = () => (
 export default function Dashboard() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [textType, setTextType] = useState(null);
+  const [isSearched, setSearch] = useState(false);
 
   return (
     <>
@@ -59,7 +55,12 @@ export default function Dashboard() {
             />
             <Input type="tel" placeholder="Enter search information" />
           </InputGroup>
-          <Button colorScheme="green" w="10%" m="2">
+          <Button
+            colorScheme="green"
+            w="10%"
+            m="2"
+            onClick={() => setSearch(true)}
+          >
             Search
           </Button>
         </HStack>
@@ -68,86 +69,14 @@ export default function Dashboard() {
           (abc@xyz.com) <br /> Search for userid - instagram, facebook, userid
           @(abc)
         </Text>
-
-        <Box pt="10">
-          <Center>
-            <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-              <LinkBox
-                as="article"
-                maxW="sm"
-                p="3"
-                borderWidth="1px"
-                rounded="md"
-              >
-                <Center mb="2" fontStyle={'semibold'}>
-                  Additional - Email
-                </Center>
-                <Card
-                  p="4"
-                  variant={'outline'}
-                  _hover={{
-                    bg: 'teal.300',
-                    // transform: transform(1.5),
-                    color: 'white',
-                    // }}
-                  }}
-                >
-                  <Heading size="md" my="2">
-                    <LinkOverlay href="/pawned" target="_blank">
-                      Check if the email has been pawned
-                    </LinkOverlay>
-                  </Heading>
-                  <Text>
-                    This also allows you to find their activity on other sites
-                  </Text>
-                </Card>
-              </LinkBox>
-
-              <LinkBox
-                as="article"
-                maxW="sm"
-                p="5"
-                borderWidth="1px"
-                rounded="md"
-              >
-                <LinkOverlay href="#">hi</LinkOverlay>
-                <Card
-                  variant={'outline'}
-                  _hover={{
-                    bg: 'teal.300',
-                    // transform: transform(1.5),
-                    color: 'white',
-                    // }}
-                  }}
-                >
-                  <CardHeader>
-                    <Heading size="md">hi</Heading>
-                  </CardHeader>
-                  <CardBody>
-                    <Text>variant = hi</Text>
-                  </CardBody>
-                </Card>
-                {/* </LinkOverlay> */}
-              </LinkBox>
-
-              <Card
-                variant={'outline'}
-                _hover={{
-                  bg: 'teal.300',
-                  color: 'white',
-                  // }}
-                }}
-              >
-                <CardHeader>
-                  <Heading size="md">hi</Heading>
-                </CardHeader>
-                <CardBody>
-                  <Text>variant = hi</Text>
-                </CardBody>
-              </Card>
-            </Grid>
-          </Center>
-        </Box>
+        {isSearched ? (
+          <Box mt="10">
+            {/* facebook */}
+            <PhoneSearch />
+          </Box>
+        ) : (
+          <BottomSearch />
+        )}
       </Box>
     </>
   );
